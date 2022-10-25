7a666d97be82e53c93d7547487e3e6e2993acdde
Xanler <tothmarcell4@gmail.com>
2022-10-25 15:12:19 +0200

    Commit 10. 25. /4 -Started branch on contour selection -Sorting contours by area and only displaying contours of a minimum and/or maximum size

--- Pages/MainPage.xaml.cs
+++ Pages/MainPage.xaml.cs
@@ -315,15 +315,63 @@ namespace tothm_szak.Pages
         {
             Mat dst = src.Clone();
             Cv2.CvtColor(src, src, ColorConversionCodes.BGR2GRAY);
-            src = simpleThreshold(src);
+            src = adaptiveThreshold(src);
+            //src = simpleThreshold(src);
             OpenCvSharp.Point[][] pl;
             HierarchyIndex[] hi;
             Scalar sc = new Scalar(0, 0, 255);
 
             Cv2.FindContours(src, out pl, out hi, RetrievalModes.Tree, ContourApproximationModes.ApproxNone, null);
+            //pl.Sort((OpenCvSharp.Point[] x, OpenCvSharp.Point[] y) => IComparer.Compare(Cv2.ContourArea(pl[x]) > Cv2.ContourArea(pl[x])));
+            sortConts(pl);
             Cv2.DrawContours(dst, pl, -1, sc, 1, LineTypes.Link8);
 
             return dst;
+        }
+
+        private OpenCvSharp.Point[][] sortConts(OpenCvSharp.Point[][] contours)
+        {
+            List<OpenCvSharp.Point[]> contourList = contours.ToList();
+            contourList.Sort((OpenCvSharp.Point[] x, OpenCvSharp.Point[] y) => CompareBySize(x, y));
+            for (int i=0; i < contours.GetLength(0); i++)
+            {
+                for (int j = 0; j < contours.GetLength(0); j++)
+                {
+                    Trace.WriteLine(Cv2.ContourArea(contours[j]));
+                }
+                Trace.WriteLine("END OF ITERATION");
+            }
+            return contours;
+        }
+
+        //this is awful but it somehow works
+        public static int CompareBySize(OpenCvSharp.Point[] x, OpenCvSharp.Point[] y)
+        {
+            if (x == null)
+            {
+                if (y == null)
+                {
+                    return 0;
+                } else
+                {
+                    return -1;
+                }
+            } else
+            {
+                if (y == null)
+                {
+                    return 1;
+                }    else
+                {
+                    if (Cv2.ContourArea(x) > Cv2.ContourArea(y))
+                    {
+                        return 1;
+                    } else
+                    {
+                        return -1;
+                    }
+                }
+            }
         }
         private Mat adaptiveThreshold(Mat src)
         {
